437b38d50899d0a2596dc1ff88af2d6ca83d9c95
ashongdev <ashongabdalla51@gmail.com>
2026-01-20 18:06:28 +0000

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -72,7 +72,12 @@ CORS_ALLOW_METHODS = [
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "https://blank-canvas-iota.vercel.app"]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "https://blank-canvas-iota.vercel.app",
+#     "https://blank-canvas-iota.vercel.app/",
+# ]
 CORS_EXPOSE_HEADERS = ["Content-Disposition"]
 ALLOWED_HOSTS = [
     "cert.ashong.dev",
