1b7d8d62a2bd9d40d67a561cab4c16722f090afe
ashongdev <ashongabdalla51@gmail.com>
2026-01-20 00:42:40 +0000

    Update CORS_ALLOWED_ORIGINS

--- server/settings.py
+++ server/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "server.urls"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "https://blank-canvas-iota.vercel.app"]
 
 ALLOWED_HOSTS = ["ashong.pythonanywhere.com"]
 
